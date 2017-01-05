c3c390d5ed960f16daef05d406e7507abe291d5a
Daniel Nordhoff-Vergien <dve@vergien.net>
2017-01-05 16:42:36 +0100

    Let MenuDemo test run with phantomJs instead of firefox
    firefox/marionete/geckodriver seems to have a problem with mouse moving

--- src/test/java/org/vaadin/addonhelpers/automated/TestMenuDemo.java
+++ src/test/java/org/vaadin/addonhelpers/automated/TestMenuDemo.java
@@ -1,25 +1,37 @@
 package org.vaadin.addonhelpers.automated;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.support.PageFactory;
 import org.vaadin.addonhelpers.manual.demo.MenuDemo;
 
+import com.github.webdriverextensions.WebDriverExtensionsContext;
+import com.github.webdriverextensions.junitrunner.WebDriverRunner;
+import com.github.webdriverextensions.junitrunner.annotations.Firefox;
+import com.github.webdriverextensions.junitrunner.annotations.IgnoreFirefox;
+import com.github.webdriverextensions.junitrunner.annotations.PhantomJS;
+import com.github.webdriverextensions.vaadin.VaadinBot;
+
+@RunWith(WebDriverRunner.class)
+@PhantomJS
+@Firefox
 public class TestMenuDemo extends AbstractWebDriverCase {
     MenuDemoPage page;
 
     @Before
     public void setup() throws MalformedURLException {
-        startBrowser();
+        startBrowser(WebDriverExtensionsContext.getDriver());
         driver.navigate().to(
                 new URL("http://localhost:5678/") + MenuDemo.class.getName());
-
+        VaadinBot.waitForVaadin();
         page = PageFactory.initElements(driver, MenuDemoPage.class);
-        waitForLoading();
         clickNotification();
     }
 
@@ -31,6 +43,7 @@ public class TestMenuDemo extends AbstractWebDriverCase {
     }
 
     @Test
+    @IgnoreFirefox
     public void testClickSubmenu() {
         page.openSub("Services").clickNavigation("Car Service");
 
@@ -39,6 +52,7 @@ public class TestMenuDemo extends AbstractWebDriverCase {
     }
     
     @Test
+    @IgnoreFirefox
     public void testClickSubSubmenu() {
         page.openSub("Beverages").openSub("Hot").clickNavigation("Tea");
 
